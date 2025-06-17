22639689c5c1e324ecf4df3b85892833b53fcbd5
abdessamadG <abdessamadgamraoui@gmail.com>
2025-06-17 16:02:21 +0100

    Discord Bot

---
+++ discord_bot.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'You are logged in as {client.user}')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!hello'):
+        await message.channel.send(f'hello {client.user}')
+      
+client.run(os.getenv('Token'))
