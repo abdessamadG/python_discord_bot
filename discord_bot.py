f3ed1f110b3775a1a9ccb5ac37e41467942dedb9
abdessamadG <abdessamadgamraoui@gmail.com>
2025-06-17 17:01:19 +0100

    Updated

--- discord_bot.py
+++ discord_bot.py
@@ -1,23 +1,21 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
     print(f'You are logged in as {client.user}')
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('!hello'):
-        await message.channel.send(f'hello {client.user}')
-      
+
+@client.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}! 👋")
+
 client.run(os.getenv('Token'))
